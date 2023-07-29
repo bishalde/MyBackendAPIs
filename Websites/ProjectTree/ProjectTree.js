21aacf9660d97dc9c70e17677d7ac716b4f6bed6
Bishal De <itsbishalde@gmail.com>
2023-07-30 00:31:09 +0530

    db-fix-1

--- Websites/ProjectTree/ProjectTree.js
+++ Websites/ProjectTree/ProjectTree.js
@@ -1,23 +1,11 @@
 const express = require("express")
 const router = express.Router()
 
-// const login=require('./Routes/login')
-// const signup=require('./Routes/signup')
+const login=require('./Routes/login')
+const signup=require('./Routes/signup')
 const users=require('./Routes/users')
 
 // require("./db/connection")
-const mongoose = require("mongoose");
-mongoose
-  .connect(process.env.PROJECTTREE_DATABASE_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("MongoDB connected for PROJECT-TREE");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
 
 router.get("/", (req, res) => {
     res.status(200).json({
@@ -26,8 +14,8 @@ router.get("/", (req, res) => {
   });
 
 
-// router.post("/signup", signup);
-// router.post("/login", login);
+router.post("/signup", signup);
+router.post("/login", login);
 router.get("/profile/:username",users)
 
 module.exports = router;
