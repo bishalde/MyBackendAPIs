3d85ee6643c1c43a374ac764f7fa823a0e338eee
Bishal De <itsbishalde@gmail.com>
2023-07-30 00:29:32 +0530

    added db

--- Websites/ProjectTree/ProjectTree.js
+++ Websites/ProjectTree/ProjectTree.js
@@ -6,6 +6,18 @@ const router = express.Router()
 const users=require('./Routes/users')
 
 // require("./db/connection")
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.PROJECTTREE_DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB connected for PROJECT-TREE");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 router.get("/", (req, res) => {
     res.status(200).json({
