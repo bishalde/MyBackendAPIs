f8ccc62b425e6cc0a6e445b521aeb33b2a852695
Bishal De <itsbishalde@gmail.com>
2023-07-30 09:26:54 +0530

    try1

--- Websites/ProjectTree/db/connection.js
+++ Websites/ProjectTree/db/connection.js
@@ -5,9 +5,13 @@ mongoose.set("strictQuery", false);
 const mongoDB = process.env.PROJECTTREE_DATABASE_URL;
 
 main()
-.then(() => console.log("Connected to MongoDB PROJECTTREE"))
-.catch((err) => console.log(err));
+  .then(() => console.log("Connected to MongoDB PROJECTTREE"))
+  .catch((err) => console.log("Error connecting to MongoDB:", err));
 
 async function main() {
-  await mongoose.connect(mongoDB);
+  try {
+    await mongoose.connect(mongoDB);
+  } catch (error) {
+    throw new Error("Failed to connect to MongoDB:", error);
+  }
 }
