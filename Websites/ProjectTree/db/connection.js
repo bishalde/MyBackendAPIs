93c2fb15dc9190b4a5c0ccc1448a7baedfe88274
Bishal De <itsbishalde@gmail.com>
2023-07-30 09:29:57 +0530

    try2

--- Websites/ProjectTree/db/connection.js
+++ Websites/ProjectTree/db/connection.js
@@ -6,12 +6,18 @@ const mongoDB = process.env.PROJECTTREE_DATABASE_URL;
 
 main()
   .then(() => console.log("Connected to MongoDB PROJECTTREE"))
-  .catch((err) => console.log("Error connecting to MongoDB:", err));
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
 
 async function main() {
   try {
     await mongoose.connect(mongoDB);
   } catch (error) {
-    throw new Error("Failed to connect to MongoDB:", error);
+    console.error("MongoDB connection error:", error);
+    // You can also log the stack trace for better debugging information
+    console.error(error.stack);
+    process.exit(1); // Exit the process with a non-zero code to indicate an error
   }
 }
+
+
+
